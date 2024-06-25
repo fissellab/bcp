b485c20dea86f1b1b5a3b085eba35e1d18205f29
fthiel99 <72046289+fthiel99@users.noreply.github.com>
2024-06-25 12:21:14 -0400

    Initial Commit

---
+++ Sag/cli_Sag.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "file_io_Sag.h"
+#include "cli_Sag.h"
+
+//This executes the commands, we can simply add commands by adding if statements
+void exec_command(char* input){
+
+	char* arg;
+	char* cmd;
+	int scan;
+	arg = (char* ) malloc(strlen(input)*sizeof(char));
+	cmd = (char* ) malloc(strlen(input)*sizeof(char));
+	scan = sscanf(input, "%s %[^\t\n]",cmd,arg);
+	if(strcmp(cmd,"print") == 0){
+
+		if(scan == 1){
+
+			printf("print is missing argument usage is print <string>\n");
+
+		}else{
+
+			printf("%s\n",arg);
+
+		}
+
+	}else if(strcmp(cmd,"exit") == 0){
+
+		printf("Exiting BCP\n");
+		exit(0);
+	}else{
+
+		printf("%s: Unknown commmand\n",cmd);
+	}
+
+}
+
+//This gets an input of arbitrary length from the command line
+char* get_input(){
+
+	char* input;
+	char c;
+	int i = 0;
+
+	input = (char *)malloc(1*sizeof(char));
+
+	while((c = getchar()) != '\n' && c != EOF){
+		input[i++] = c;
+		input = (char* )realloc(input,i+1);
+
+	}
+
+	input[i] = '\0';
+	return input;
+}
+
+//This is the main function for the command line
+void cmdprompt(FILE* cmdlog){
+
+	int count = 1;
+	char* input;
+
+	while(1){
+
+		printf("[BCP@Saggitarius]<%d>$ ",count);
+		input = get_input();
+		if(strlen(input)!=0){
+			write_to_log(cmdlog,"cli.c","cmdprompt",input);
+			exec_command(input);
+		}
+		count++;
+	}
+}
+
---
+++ Sag/cli_Sag.h
@@ -0,0 +1,8 @@
+#ifndef CLI_SAG_H
+#define CLI_SAG_H
+
+void cmdprompt(FILE* cmdlog);
+void exec_command(char* input);
+char* get_input();
+#endif
+
