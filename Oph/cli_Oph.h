030d21915aea2e359400092c8175941c47755954
fthiel99 <72046289+fthiel99@users.noreply.github.com>
2024-06-25 12:25:56 -0400

    Initial Commit

---
+++ Oph/cli_Oph.h
@@ -0,0 +1,8 @@
+#ifndef CLI_OPH_H
+#define CLI_OPH_H
+
+void cmdprompt(FILE* cmdlog);
+void exec_command(char* input);
+char* get_input();
+#endif
+
---
+++ Oph/file_io_Oph.h
@@ -0,0 +1,19 @@
+#ifndef FILE_IO_OPH_H
+#define FILE_IO_OPH_H
+
+void write_to_log(FILE* fptr, char* cfile, char* func, char* msg);
+void read_in_config(char* filepath);
+void print_config();
+
+typedef struct main_conf{
+	char* logpath;
+	char* cmdlog;
+} main_conf;
+
+typedef struct conf_params{
+	struct main_conf main;
+} conf_params;
+
+extern struct conf_params config;
+#endif
+
