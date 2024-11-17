3e1f1d0d3d299518d8637269c14c51437ae65e9e
Oliver Dantzer <oliverdantzer@gmail.com>
2024-11-16 22:21:24 -0500

    moved source files into src/, improves docs

--- Oph/compile_code.sh
+++ Oph/compile_code.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-gcc -g main_Oph.c file_io_Oph.c cli_Oph.c bvexcam.c camera.c lens_adapter.c matrix.c astrometry.c accelerometer.c ec_motor.c motor_control.c\
+gcc -g src/main_Oph.c src/file_io_Oph.c src/cli_Oph.c src/bvexcam.c src/camera.c src/lens_adapter.c src/matrix.c src/astrometry.c src/accelerometer.c src/ec_motor.c src/motor_control.c\
     -lpthread -lconfig -lcurses -lueye_api -lm -lastrometry -lglib-2.0 -o bcp_Oph /usr/local/lib/libsofa_c.a /usr/local/lib/libsoem.a\
-    -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include \
-    -L..bvex-link/telemetry-uplink/build/vcpkg_installed/x64-linux/lib \
-    -I..bvex-link/telemetry-uplink/build/vcpkg_installed/x64-linux/include/nanopb \
-    -llibprotobuf-nanopb \
-    -I../bvex-link/telemetry-uplink/include -L../bvex-link/telemetry-uplink/build \
-    -ltelemetry-uplink
+    -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable 'bcp_Oph' created."
--- Oph/install_deps.sh
+++ Oph/install_deps.sh
@@ -1,5 +1,4 @@
 sudo apt update
 sudo apt install -y libpthread-stubs0-dev libconfig-dev  \
-    libncurses5-dev libncursesw5-dev libueye-dev  \
-    libastrometry-dev libglib2.0-dev
+    libncurses5-dev libncursesw5-dev libglib2.0-dev libomp5
 ./installation/install_pkgs.sh
